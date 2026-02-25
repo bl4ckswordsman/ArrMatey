0beee3087e58a38716d5501b93f81ec17d53ee15
Owen LeJeune <omlejeune@gmail.com>
2026-02-25 11:33:18 -0500

    disable seerr for now

--- shared/src/commonMain/kotlin/com/dnfapps/arrmatey/compose/TabItem.kt
+++ shared/src/commonMain/kotlin/com/dnfapps/arrmatey/compose/TabItem.kt
@@ -6,19 +6,20 @@ import dev.icerock.moko.resources.StringResource
 enum class TabItem(
     val iosIcon: String,
     val resource: StringResource,
-    val drawerOnly: Boolean = false
+    val drawerOnly: Boolean = false,
+    val isDisabled: Boolean = false // should only be set for in-progress views
 ) {
     SHOWS("tv", MR.strings.series),
     MOVIES("movieclapper", MR.strings.movies),
     MUSIC("music.quarternote.3", MR.strings.music),
     ACTIVITY("square.and.arrow.down", MR.strings.activity),
     CALENDAR("calendar", MR.strings.schedule),
-    REQUESTS("tray.fill", MR.strings.requests),
+    REQUESTS("tray.fill", MR.strings.requests, isDisabled = true),
 
     SETTINGS("gear", MR.strings.settings, drawerOnly = true);
 
     companion object {
-        val navigationEntries = entries.filter { !it.drawerOnly }
+        val navigationEntries = entries.filter { !it.drawerOnly && !it.isDisabled }
         val defaultEntries = listOf(SHOWS, MOVIES, MUSIC, ACTIVITY, CALENDAR)
         val defaultHidden = navigationEntries.filter { !defaultEntries.contains(it) }
     }
